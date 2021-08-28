2802c7ecfd6f19898394a5e1f7346cb2405548a6
Aniket Pandharabale <aniketicloud@gmail.com>
2021-08-28 20:40:57 +0530

    fixed typos and added readme for NOTES

--- ecomm/products/webpack.config.js
+++ ecomm/products/webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
       name: 'products',
       filename: 'remoteEntry.js',
       exposes: {
-        './ProductIndex': './src/index',
+        './ProductsIndex': './src/index',
       },
     }),
     new HtmlWebpackPlugin({
