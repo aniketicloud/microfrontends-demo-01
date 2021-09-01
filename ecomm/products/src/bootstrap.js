3c8fccb2b3f9c92c4f950f15d121108f5a8b61c7
Aniket Pandharabale <aniketicloud@gmail.com>
2021-09-01 23:33:49 +0530

    Sub-App  execution status
    The problem with document id selector.
    Also there is one bug with id selector. Like how module federation creates an variable of the name in js file. This overrides js content with html content.

--- ecomm/products/src/bootstrap.js
+++ ecomm/products/src/bootstrap.js
@@ -8,3 +8,21 @@ for (let i = 0; i < 5; i++) {
 }
 
 document.querySelector('#dev-products').innerHTML = products;
+
+// ! Suppose (In Reality actually), different teams are responsible for different project 
+// document.querySelector('#dev-products').innerHTML = products;
+// ? here, Remote project cannot control over Host project to have same id.
+
+/**
+ * Situation #1
+ * We're running this file in 'Development' in isolation (as standalone application)
+ * We're using 'local' `index.html` file, which always will have an element
+ * of id `dev-product`.
+ * We want to immediately render our app into that element
+ */
+
+/**
+ * We're running this file (Remote) in 'Development' or 'Production' through Host
+ * NO GURANTEE that an element with an id of `dev-products` exists
+ * WE DO NOT WANT to try to immediately render the app
+ */
