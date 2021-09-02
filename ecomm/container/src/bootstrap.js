7c5d2a2e3b9460d1f8255b46fab81cd21ef81e76
Aniket Pandharabale <aniketicloud@gmail.com>
2021-09-02 19:33:47 +0530

    Consuming Remote module: products

--- ecomm/container/src/bootstrap.js
+++ ecomm/container/src/bootstrap.js
@@ -1,4 +1,8 @@
-import 'products/ProductsIndex';
+import { mount } from 'products/ProductsIndex';
 import 'cart/CartShow';
 
 console.log('Container .. !');
+
+const el = document.querySelector('#my-products');
+
+mount(el);
